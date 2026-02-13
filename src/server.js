bb484e7091c7891852793b6b75f39f17379807fb
Vidharshana06-web <vidharshanadevaraj06@gmail.com>
2026-02-13 22:34:21 +0530

    last commit

--- src/server.js
+++ src/server.js
@@ -39,7 +39,7 @@ app.use(cors({
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
 }));
-app.options("*", cors());
+
 
 // Body parsers
 app.use(express.json({ limit: '10mb' }));
